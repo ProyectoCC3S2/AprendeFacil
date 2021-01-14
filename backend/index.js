0ca96f02714eb312829837737435cd649f2a0cef
Danilo Blas <sdann_26@hotmail.com>
2021-01-13 19:03:05 -0500

    Back v4

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,23 @@
 //libraries
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+const morgan = require('morgan') // Permite registra o ver por consola las peticiones que llegan desde el navegador
+const cors = require('cors') 
 const bodyparser = require('body-parser')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // Permite conectarnos a la base de datos y al mismo tiempo definir como luciran los datos dentro de la base de datos
 
 
-//methods libs
+// methods libs
 const app = express()
 require('dotenv').config();
 
 
-//middlewares
+// middlewares
 app.use(morgan('dev'));
-
 app.use(bodyparser.json());
+//app.use(express.json());
 app.use(cors())
-//database setup
+
+// database setup
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser:true,
     useUnifiedTopology:true,
@@ -27,6 +28,8 @@ mongoose.connect(process.env.DATABASE, {
 //routes setup
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/publicacion', require('./routes/publicacion'))
+//app.use('/api/solucion', require('./routes/solucion'))
+//app.use('/api/notificacion', require('./routes/notificacion'))
 
 //listen port
 const port = process.env.PORT;
