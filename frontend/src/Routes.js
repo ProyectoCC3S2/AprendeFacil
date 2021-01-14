aaf05f618da8211d418bb952cda9961f9613f118
GabrielVicenteAlva <42977334+GabrielVicenteAlva@users.noreply.github.com>
2021-01-14 00:43:08 -0500

    Update Routes.js

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -14,6 +14,9 @@ import CreatePost from "./components/CreatePost";
 import Prueba from './components/Prueba';
 import Profile from './components/Profile'
 import PostDone from './components/PostDone';
+import RecargarMonedas from './components/RecargarMonedas'
+import ConvertirMonedas from './components/ConvertirMonedas';
+
 
 export  const ProtectedComponent = () => {
     return <Redirect to='http://localhost:3000/'/>
