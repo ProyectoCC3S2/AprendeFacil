642f57f940d126a4d3f30c9d1a2d67f3282088ad
Danilo Blas <sdann_26@hotmail.com>
2021-02-04 07:21:02 -0500

    Tercera
    v10

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup, signin, signout } = require('../controllers/authController');
+const { signup, signin, updateMoney } = require('../controllers/authController');
 
 router.post('/signup', signup);
 router.post('/signin', signin);
-/*
-router.get('/protected', protegido);
-router.post('/signout', signout);
-*/
+router.put('/:authId', updateMoney);
 
 module.exports = router;
